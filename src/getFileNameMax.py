06d447b884d89a4b141aee769c4b7356bc5877c1
Alex Lubchuk <alexey.lubchuk@gmail.com>
2020-07-30 17:57:48 +0300

    Create a demo to showcase a bug with ENAMETOOLONG

---
+++ src/getFileNameMax.py
@@ -0,0 +1,3 @@
+import os
+
+print(os.pathconf('/', 'PC_NAME_MAX'))
---
+++ src/index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+const spawn = require("child_process").spawnSync;
+
+try {
+  const pathMaxProcess = spawn('python',["./getPathMax.py"]);
+  const fileNameMaxProcess = spawn('python',["./getFileNameMax.py"]);
+  const MAX_PATH = parseInt(pathMaxProcess.stdout.toString("utf-8"));
+  const MAX_FILENAME = parseInt(fileNameMaxProcess.stdout.toString("utf-8"));
+  console.log({ MAX_PATH, MAX_FILENAME })
+
+  const paths = fs.readdirSync("./config")
+  const longPath = paths.map(p => path.resolve(p)).join(path.delimiter)
+  const runtimeJSON = "/runtime.json";
+  console.log({ longPath, longPathLength: longPath.length + runtimeJSON.length })
+
+  process.env.NODE_CONFIG_DIR = longPath;
+} catch(e) {
+  console.log("Error happened: " + e)
+}
+
+require('config');
